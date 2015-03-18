2efa8b6de5a2b2e9a024d14875a2df46b2e7bd04
Sean Snell <ssnell1@memphis.edu>
2015-03-18 16:38:12 -0500

    finally got add units working properly

--- authoring_tool.js
+++ authoring_tool.js
@@ -1,27 +1,45 @@
 if (Meteor.isClient) {
+	unitList = new Mongo.Collection('unitList');
+	Session.setDefault('numUnitsInt', 0);
+	
+
+	
   Template.hello.helpers({
 
   });
 
   Template.Units.helpers({
+    unitList: function () {
+      return unitList.find({});
+    },
     unitVal: function () {
       return Session.get('unitVal');
     },
-    instructions: function() {
+    instruction: function(toReturn) {
+	
+	//var attrclass = $(event.target).attr("class").split('m')[1];
+	console.log("unit num... " + toReturn)
+	//var x = unitList.findOne({num: parseInt(unitNum)}).instructions;
+	return toReturn;
+
 	var v = Session.get('unitVal');	
 	if (v == 'instruction')
 	    return true;
 	else
 	    return false;
     },
-    assessment: function() {
+    assessment: function(toReturn) {
+	console.log("unit num... " + toReturn)
+	return toReturn;
 	var v = Session.get('unitVal');	
 	if (v == 'assessment')
 	    return true;
 	else
 	    return false;
     },
-    learningsession: function() {
+    learningsession: function(toReturn) {
+	console.log("unit num... " + toReturn)
+	return toReturn;
 	var v = Session.get('unitVal');	
 	if (v == 'learningsession')
 	    return true;
@@ -33,12 +51,27 @@ if (Meteor.isClient) {
   Template.hello.events({
     'change .unitType': function (event) {
       var v = $(event.target).val();
-      console.log("unit type running");
+	var attrclass = $(event.target).attr("class").split('m')[1];
+      //console.log(attrclass);
+      var x = unitList.findOne({num: parseInt(attrclass)});
+	
       if (v == "instruction") {
+	unitList.update({_id: x._id}, {$set: {instructions: true}});
+	unitList.update({_id: x._id}, {$set: {learningsessions: false}});
+	unitList.update({_id: x._id}, {$set: {assessments: false}});
+
       	Session.set('unitVal', 'instruction');
       } else if (v == "learningsession") {
+	unitList.update({_id: x._id}, {$set: {instructions: false}});
+	unitList.update({_id: x._id}, {$set: {learningsessions: true}});
+	unitList.update({_id: x._id}, {$set: {assessments: false}});
+
       	Session.set('unitVal', 'learningsession');
       } else if (v == "assessment") {
+	unitList.update({_id: x._id}, {$set: {instructions: false}});
+	unitList.update({_id: x._id}, {$set: {learningsessions: false}});
+	unitList.update({_id: x._id}, {$set: {assessments: true}});
+
       	Session.set('unitVal', 'assessment');
       } else {
       	Session.set('unitVal', 'invalid');
@@ -48,10 +81,29 @@ if (Meteor.isClient) {
 	var v = $(event.target).val();
     }
   });
+
+  Template.Units.events({
+    'click .addUnit': function(event) {
+      console.log("add unit button clicked");
+	Session.set('numUnitsInt', Session.get('numUnitsInt') + 1);
+	unitList.insert({num:Session.get('numUnitsInt'),
+				instructions:false,
+				learningsessions:false,
+				assessments:false});
+    }
+
+  });
 }
 
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  unitList = new Mongo.Collection('unitList');
+  //Session.setDefault('numUnitsInt', 0);
+  Meteor.startup(function() {
     // code to run on server at startup
+	unitList.remove({})
+   
+
   });
+
 }
